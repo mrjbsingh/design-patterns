436b553f4d058aed842077f7e82c7efb35e62ef0
mrjbsingh <jayendra.iiita@gmail.com>
2022-05-07 09:47:40 +0530

    double check locking singleton implemented

--- singleton/DemoMultiThread.java
+++ singleton/DemoMultiThread.java
@@ -1,31 +1,31 @@
 package singleton;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DemoMultiThread {
 
     public static void main(String[] args) {
 
-        Thread threadJay = new Thread(new ThreadJay());
-        Thread threadVeru = new Thread(new ThreadVeru());
-        threadJay.start();
-        threadVeru.start();
-    }
 
-    static class ThreadJay implements Runnable{
-
-        @Override
-        public void run() {
-            NaiveSingleton obj1 = NaiveSingleton.getNaiveSingleton("jay");
-            System.out.println(obj1.getValue());
+        ArrayList<Thread> threads = new ArrayList<>();
+        for(int i=1; i<=10; i++){
+            threads.add(new Thread(new ThreadWithName("jay "+i)));
         }
+        for ( Thread thread: threads ) {
+            thread.start();
+        }
+
     }
 
-    static class ThreadVeru implements Runnable{
-
+    static class ThreadWithName implements Runnable{
+        private String threadName;
+        public ThreadWithName(String threadName){
+            this.threadName = threadName;
+        }
         @Override
         public void run() {
-            NaiveSingleton obj1 = NaiveSingleton.getNaiveSingleton("veru");
+            NaiveSingleton obj1 = NaiveSingleton.getNaiveSingleton(this.threadName);
             System.out.println(obj1.getValue());
         }
     }
--- singleton/NaiveSingleton.java
+++ singleton/NaiveSingleton.java
@@ -5,14 +5,25 @@ public class NaiveSingleton {
     private String value;
 
     private NaiveSingleton(String value){
+        try{
+            Thread.sleep(1000);
+        }catch (InterruptedException ex){
+            ex.printStackTrace();
+        }
         this.value = value;
     }
 
     public static NaiveSingleton getNaiveSingleton(String value){
-        if(instance==null){
-            instance = new NaiveSingleton(value);
+        if(instance!=null){
+            return instance;
         }
-        return instance;
+        synchronized (NaiveSingleton.class){
+            if(instance==null){
+                instance = new NaiveSingleton(value);
+            }
+            return instance;
+        }
+
     }
 
     public String getValue() {
